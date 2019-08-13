97dcd4134f968f8cff502c60af6f4413d50e2261
mmacdonald1 <melmacd94@gmail.com>
2019-08-13 18:08:30 -0400

    cards setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  deck=[]
   title = 'angular-cards';
+
+  shuffle=()=>{
+    this.deck=[{suit:"spades", value:"4"},{suit:"hearts", value:"Queen"}]
+  }
+  makeDeck=()=>{
+    this.deck = [{suit:"hearts", value:"Queen"},{suit:"spades", value:"4"}]
+  }
 }
