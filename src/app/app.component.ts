99dfdcbc8839e70d6407551d2c02c08ca85bdbc2
mmacdonald1 <melmacd94@gmail.com>
2019-08-13 20:12:42 -0400

    cards with images in and shuffling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,36 @@ export class AppComponent {
   deck=[]
   title = 'angular-cards';
 
+
   shuffle=()=>{
-    this.deck=[{suit:"spades", value:"4"},{suit:"hearts", value:"Queen"}]
+    let i = this.deck.length
+    let j
+    let temp
+    //from the back loop through until last letter
+    while(--i > 0){
+      //number that you will switch with current number
+      j = Math.floor(Math.random()*(i+1))
+      temp=this.deck[j]
+      //swap j for i
+      this.deck[j] = this.deck[i]
+      //swap i for j
+      this.deck[i] = temp
+    }
   }
   makeDeck=()=>{
-    this.deck = [{suit:"hearts", value:"Queen"},{suit:"spades", value:"4"}]
-  }
+    let values=["2","3","4","5","6","7","8","9","10","J","Q","K", "A"]
+    let suits=["H", "S", "D", "C"]
+    let i = 0
+    for(let j=0; j<13; j++){
+     for(let k=0; k<4; k++){
+       this.deck[i] = {}
+       this.deck[i].id=i+1
+       this.deck[i].value = values[j]
+       this.deck[i].suit= suits[k]
+       this.deck[i].img= "assets/images/"+values[j]+suits[k]+".png"
+        ++i
+     }
+    }
+   }
+
 }
