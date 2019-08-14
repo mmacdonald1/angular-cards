3de06759d7213eb7694ab1668cc4a46fc5ee00b4
mmacdonald1 <melmacd94@gmail.com>
2019-08-13 20:55:58 -0400

    tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -11,7 +13,10 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+    });
   }));
 
   it('should create the app', () => {
@@ -19,17 +24,28 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'angular-cards'`, () => {
+  it(`should have a button 'shuffle'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-cards');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button').textContent).toEqual('Shuffle');
   });
-
-  it('should render title in a h1 tag', () => {
+  it('should render 52 cards', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-cards!');
+    expect(compiled.querySelectorAll('.playingCard').length).toBe(52);
   });
+  it('should shuffle on shuffle button press', async(() => {
+      spyOn(component, 'shuffle');
+
+      let unshuffledDeck= component.deck
+
+      let button = fixture.debugElement.nativeElement.querySelector('button');
+      button.click();
+
+      fixture.whenStable().then(() => {
+        expect(component.shuffle).toHaveBeenCalled();
+      });
+   }));
+
 });
